5fd96018d5bcc029a04c9a5ed0b3fbdf37f0f3c1
justy031090 <justy031090@gmail.com>
2021-12-10 23:45:56 +0200

    update

--- covid.js
+++ covid.js
@@ -21,12 +21,6 @@ Solution
 The solution is to call our countries api and choose from their the specific 
 continent you want the list of countries from and compare those countries to 
 our covid 19 countries.
-!cca2 (country API && region API) === code(covid API)
-2. The statistics we want from each country is the following:
-- Confirmed Cases //!from covid API
-- Number of Deaths  //!from covid API
-- Number of recovered   //!from covid API
-- Number of critical condition  //!from covid API
 
 We want the ability to choose what statistics we want to see and display them
 on a graph.  //!learn the library
@@ -40,6 +34,11 @@ include at least the following:
 - new deaths
 - total recovered
 */
+
+
+
+
+//getting the data for each continent API
 let oceania = [];
 let europe= [];
 let asia = [];
@@ -47,8 +46,9 @@ let americas =[];
 let africa = [];
 let covInf = [];
 
-//getting the data for each continent API
+
 function getRegion (array, region) {
+    try{
     fetch(`https://intense-mesa-62220.herokuapp.com/https://restcountries.herokuapp.com/api/v1/region/${region}`)
     .then(function(res){
         return res.json()
@@ -57,7 +57,12 @@ function getRegion (array, region) {
         return data.forEach(el=>array.push(el))
     })
     return array
+    }
+    catch(e){
+        console.log('oh no', e)
+        }
 }
+
 getRegion(asia, 'asia')
 getRegion(europe, 'europe')
 getRegion(americas, 'americas')
@@ -67,29 +72,126 @@ getRegion(africa, 'africa')
 
 //getting all the data from Covid API
 async function getCovData ()  {
+    try{
     let inf = await (await fetch ('https://corona-api.com/countries')).json()
     return  inf.data.forEach(el=>covInf.push(el))
+    }
+    catch(e){
+        console.log('oh no', e)
+    }
 }
-getCovData()
-
 //get latest data for each continent
-function regionCases(region){
-    let cases = {};
-setTimeout(()=> {
+let latestData = []
+let countryName =[]
+async function regionCases(region, strRegion){
+    await getRegion(region, strRegion)
+    await getCovData()
     for(let i=0; i<covInf.length; i++){
         for(let j=0; j<region.length; j++){
             if(region[j].cca2 === covInf[i].code){
-                let myString = JSON.stringify(region[j].name.common)
-                cases[myString] = covInf[i].latest_data
+                latestData.push(covInf[i].latest_data);
+                countryName.push(region[j].name.common);
             }
         } 
-    } return cases
-}, 500)
-return cases
+    } return countryName, latestData
 }
 
-console.log(regionCases(africa))
+setTimeout(()=>{
+    regionCases(africa, 'africa')
+}, 500)
+
+//counting all cases per country & per continent
+let deaths= [];
+let totalDeaths = 0;
+(function countDeaths (){
+setTimeout(()=>{
+    for (let i=0; i<latestData.length; i++){
+        deaths.push(latestData[i].deaths)
+        totalDeaths += latestData[i].deaths
+        } return totalDeaths, deaths
+}, 1000)
+})()
+let confirmed= [];
+let totalConfirmed = 0;
+(function countConfirmed (){
+setTimeout(()=>{
+    for (let i=0; i<latestData.length; i++){
+        confirmed.push(latestData[i].confirmed)
+        totalConfirmed += latestData[i].confirmed
+        } return totalConfirmed, confirmed
+}, 1000)
+})()
+let recovered= [];
+let totalRecovered = 0;
+(function countRecovered  (){
+setTimeout(()=>{
+    for (let i=0; i<latestData.length; i++){
+        recovered.push(latestData[i].recovered)
+        totalRecovered += latestData[i].recovered
+        } return totalRecovered, recovered
+}, 1000)
+})()
+let critical= [];
+let totalCritical = 0;
+(function countCritical  (){
+setTimeout(()=>{
+    for (let i=0; i<latestData.length; i++){
+        critical.push(latestData[i].critical)
+        totalCritical += latestData[i].critical
+        } return totalCritical, critical
+}, 1000)
+})()
+
+setTimeout(()=>{
+console.log(deaths)
+console.log(totalDeaths)
+console.log(critical)
+console.log(totalCritical)
+console.log(recovered)
+console.log(totalRecovered)
+console.log(confirmed)
+console.log(totalConfirmed)
+}, 1000)
 
 
+// getting started with html
+let canvas = document.querySelector('canvas');
+setTimeout(()=>{
+const myChart = new Chart(canvas, {
+    type: 'pie',
+    data: {
+        labels: countryName,
+        datasets: [{
+            label: 'Deaths',
+            data: deaths,
+            backgroundColor: [
+                'rgb(218, 112, 214)',
+                'orchid',
+                'pink',
+                'orange',
+                'yellow',
+                'green',
+                'blue',
+                'purple',
+            ],
+            borderWidth: 1
+        }]
+    },
 
-
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        scales: {
+            x: {
+                ticks:{
+                    autoSkip: false
+                }
+            }
+        }
+        
+    }
+});
+}, 1500)
